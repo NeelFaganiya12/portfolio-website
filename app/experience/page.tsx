42cfa188b6b6bb65de8c07a0ff2c9370054e36cd
Neel Sanjaybhai Faganiya <neel.f2002@gmail.com>
2026-04-19 15:16:54 -0400

    fix: update experience

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,4 +1,5 @@
 const experience = [
+  { role: "Graduate Researcher", org: "CrySP (Cryptography, Security and Privacy) Lab, University of Waterloo", dates: "September 2025 – Present" },
   { role: "Cyber Security Analyst", org: "Cryptography & Certificate Services", dates: "May 2024 – August 2024" },
   { role: "Cyber Security Analyst", org: "Cryptography & Certificate Services", dates: "May 2023 – August 2023" },
   { role: "Cyber Security Analyst", org: "Global IT Risk", dates: "September 2022 – April 2023" },
